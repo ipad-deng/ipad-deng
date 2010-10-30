a7bc7217af796a004faf19b1eb4e6b60b73b9aca
ipad-deng <ipad.deng@gmail.com>
2010-10-30 22:54:56 +0800

    first comment

---
+++ ipad-deng/CHMView/Entries/2010/10/19_2.view_chms_in_iPad_files/19_2.view_chms_in_iPad.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.1 local-build-20101030
+
+setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-3,3,6,182),url:'19_2.view_chms_in_iPad_files/stroke.png'},{rect:new IWRect(-3,-3,6,6),url:'19_2.view_chms_in_iPad_files/stroke_1.png'},{rect:new IWRect(3,-3,244,6),url:'19_2.view_chms_in_iPad_files/stroke_2.png'},{rect:new IWRect(247,-3,6,6),url:'19_2.view_chms_in_iPad_files/stroke_3.png'},{rect:new IWRect(247,3,6,182),url:'19_2.view_chms_in_iPad_files/stroke_4.png'},{rect:new IWRect(247,185,6,6),url:'19_2.view_chms_in_iPad_files/stroke_5.png'},{rect:new IWRect(3,185,244,6),url:'19_2.view_chms_in_iPad_files/stroke_6.png'},{rect:new IWRect(-3,185,6,6),url:'19_2.view_chms_in_iPad_files/stroke_7.png'}],new IWSize(250,188))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('19_2.view_chms_in_iPad_files/19_2.view_chms_in_iPadMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');fixAllIEPNGs('../../../../Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();fixupIECSS3Opacity('id4');BlogFixupPreviousNext();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
