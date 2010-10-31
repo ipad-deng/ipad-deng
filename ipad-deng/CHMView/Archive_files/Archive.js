dd7a4952a97ff7a91719966dec898f120f836099
ipad-deng <ipad.deng@gmail.com>
2010-10-31 11:03:29 +0800

    updated 10.0.2.2 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA1Yp9qmevaV5i5Uo9yA9IgpZir24nbM3V1Nu1SFUjQ88IZ0SV3yLcVi/uefqn9x7IEQCbZQVFEEZ6fGG3wFunTs0jeR74+MhI7XkK9ffBr/B38+g1Wshe8q2KDuR/IQ2QItuUgpjKVtfXHJhuxKwocLGeOAJjeyAaXslPBgb7rAx59Ni7jpqCa7ELSAM6yimlS8CdLpFLxrbL4ALTuwN4l2n3QmhGX+PvCJZFult0amrKBH2nSoAUTjUj5Y+OZMd0hqYp03AmwxtQXB/tzRNBYgiTNZEAO/x2lYfOYrDFlWB6Vu48H+9w/rzMv6oOkbmW4XNp0tpqBRd6fUSTBI42jw==

--- ipad-deng/CHMView/Archive_files/Archive.js
+++ ipad-deng/CHMView/Archive_files/Archive.js
@@ -1,9 +1,9 @@
-// Created by iWeb 3.0.1 local-build-20101030
+// Created by iWeb 3.0.1 local-build-20101031
 
 setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Archive_files/ArchiveMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('../Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();fixupIECSS3Opacity('id3');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('../Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
