3a37abaa6d83fa70ddeda2ef790e284a8d920a3b
ipad-deng <ipad.deng@gmail.com>
2010-11-11 05:16:58 +0800

    add CHMView

--- ipad-deng/CHMView/Entries/2010/10/31_View_the_contents_files/31_View_the_contents.js
+++ ipad-deng/CHMView/Entries/2010/10/31_View_the_contents_files/31_View_the_contents.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.1 local-build-20101031
+// Created by iWeb 3.0.1 local-build-20101111
 
 function writeMovie1()
 {detectBrowser();if(windowsInternetExplorer)
@@ -13,6 +13,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {dynamicallyPopulate();loadMozillaCSS('31_View_the_contents_files/31_View_the_contentsMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');detectBrowser();adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');fixAllIEPNGs('../../../../Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();BlogFixupPreviousNext();applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');fixAllIEPNGs('../../../../Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();BlogFixupPreviousNext();applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
---
+++ ipad-deng/CHMView_Support_files/CHMView_Support.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.1 local-build-20101111
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('CHMView_Support_files/CHMView_SupportMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
